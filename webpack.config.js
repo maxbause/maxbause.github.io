8c09bb6696eb6f6846ce276425ffd23c60312833
Max <maxbause@gmx.de>
2020-12-04 22:05:22 +0100

    refactor: webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,8 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 3000,
+    publicPath: "/dist/",
   },
 }
